711da552882f4b543f019532b3d656f34c210549
Dindro <31180301+Dindro@users.noreply.github.com>
2018-01-30 23:07:52 +0300

    start

--- services/index.js
+++ services/index.js
@@ -1,15 +1,16 @@
-require('module-alias/register');
+require("module-alias/register");
 const express = require("express"),
     app = express(),
-    bodyParser = require('body-parser'),
-    cors = require('cors'),
-    http = require('http');
+    bodyParser = require("body-parser"),
+    cors = require("cors"),
+    http = require("http"),
+    server = http.Server(app),
+    io = require("socket.io")(server);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false })); //Аналогично предыдущему, только 'Content-Type' равен 'application/x-www-form-urlencoded'. Параметр { extended: false } означает обработку параметров тела запроса как строки или массива.
+app.use(bodyParser.json()); //Парсит тело только тех запросов, для которых 'Content-Type' равен 'application/json'. Результат парсинга сохраняется в объекте req.body
+app.use(cors()); //Доступность с любого домена
 
-const server = http.Server(app);
 const port = process.env.PORT || 3000;
 server.listen(port, function () {
     console.log(`Tasks service running on ${port}`);
