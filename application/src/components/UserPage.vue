4c993c18ee44bcbf290c6fccb656cfed8ea4171a
Dindro <31180301+Dindro@users.noreply.github.com>
2018-04-19 03:47:42 +0300

    scroll подточен под идеал

--- application/src/components/UserPage.vue
+++ application/src/components/UserPage.vue
@@ -4,84 +4,78 @@ export default {
   data() {
     return {
       lastYScrollPos: 0,
-      lastDownScrollPos: 0,
-      lastUpScrollPos: 0,
-      upLeftPos: 0,
-      downLeftPos: 0
+      upPos: 0,
+      downPos: 0
     };
   },
   methods: {
     scroll(e) {
-      const bottom = 15;
-      const topScreen = 70;
-      const YScrollPos = e.currentTarget.pageYOffset;
+      // условия - класс элемента не должен фиксироваться
+      const boxID = "left";
+      const boxTop = 70;
+      const boxBottom = 15;
+      const YScrollPos = e.currentTarget.scrollY;
 
-      const left = document.getElementById("left");
-      const leftHeight = left.clientHeight;
-      const leftFullHeight = leftHeight + topScreen + bottom;
-      const rodeHeight = leftFullHeight - window.innerHeight;
+      // через document, потомучто учитывается без скролл бара
+      const winHeight = document.documentElement.clientHeight;
 
-      // высота экрана больше чем
-      if (leftFullHeight > window.innerHeight) {
-        // скролл вниз?
+      const box = document.getElementById(boxID);
+      const boxHeight = box.clientHeight;
+      const boxFullHeight = boxHeight + boxTop + boxBottom;
+      const boxInvisibleHeight = boxFullHeight - winHeight;
+
+      // условие при первом запуске
+      if (this.downPos === 0) {
+        this.downPos = boxInvisibleHeight + YScrollPos;
+      }
+
+      // высота экрана больше чем высота блока
+      const isBoxOnScreen = boxFullHeight > winHeight;
+
+      // позиция скрола поменялась
+      const isScrollChange = this.lastYScrollPos !== YScrollPos;
+      if (isBoxOnScreen && isScrollChange) {
+        // скролл вниз
         const isToDown = this.lastYScrollPos < YScrollPos;
         if (isToDown) {
-          // const isRode = this.lastUpScrollPos + rodeHeight < YScrollPos;
-          const isRode = (() => {
-            const rode = this.upLeftPos + rodeHeight;
-            const isRodeCenter = rode < YScrollPos;
+          const rodePos = this.upPos + boxInvisibleHeight;
+          const isRodeDown = rodePos < YScrollPos;
 
-            return isRodeCenter /* YScrollPos > rodeHeight || YScrollPos  */;
-          })();
-          const isCenter =
-            /* this.lastUpScrollPos */ this.upLeftPos - rodeHeight > 0;
+          if (isRodeDown) {
+            // скрол проехал
+            box.classList.add("fixed");
+            box.style.bottom = `${boxBottom}px`;
+            box.style.marginTop = "";
 
-          console.log("isRode ", isRode);
-          console.log("isCenter ", isCenter);
-
-          // скрол проехал?
-          if (isRode) {
-            left.classList.add("fixed");
-            left.style.bottom = `${bottom}px`;
-            left.style.marginTop = "";
-            this.downLeftPos = YScrollPos;
-          } else if (isCenter) {
-            // если скролл в центре
-            const leftMarginTop = left.style.marginTop;
-
-            if (leftMarginTop === "") {
-              const marginTop = this.upLeftPos;
-              left.style.marginTop = `${marginTop}px`;
-            }
-            left.classList.remove("fixed");
+            // при фиксации меняем позиции
+            this.upPos = YScrollPos - boxInvisibleHeight;
+            this.downPos = YScrollPos;
           } else {
-            left.classList.remove("fixed");
+            // скрол не проехал
+            if (box.style.marginTop === "") {
+              box.style.marginTop = `${this.upPos}px`;
+            }
+            box.classList.remove("fixed");
           }
-
-          // Фиксируем позицию
-          this.lastDownScrollPos = YScrollPos;
         } else {
-          // иначе: скролл вверх
-					//const rodeUpHeight = this.downLeftPos - rodeHeight;
-					/* this.lastDownScrollPos */
-          const isRodeUp = (() => {
-            return this.downLeftPos - rodeHeight > YScrollPos;
-          })();
-					console.log('isRodeUP ',isRodeUp);
+          // скролл наверх
+          const isRodeUp = this.upPos > YScrollPos;
+
           if (isRodeUp) {
-            left.classList.add("fixed");
-            left.style.marginTop = "";
-            this.upLeftPos = YScrollPos;
+            // скрол проехал
+            box.classList.add("fixed");
+            box.style.marginTop = "";
+
+            this.upPos = YScrollPos;
+            this.downPos = YScrollPos + boxInvisibleHeight;
           } else {
-            if (left.style.marginTop === "") {
-              //const marginTop = this.lastDownScrollPos - rodeHeight;
-              const marginTop = this.downLeftPos - rodeHeight;
-              left.style.marginTop = `${marginTop}px`;
+            if (box.style.marginTop === "") {
+              const boxMarginTop = this.downPos - boxInvisibleHeight;
+              box.style.marginTop = `${boxMarginTop}px`;
             }
-            left.style.bottom = "";
-            left.classList.remove("fixed");
+            box.style.bottom = "";
+            box.classList.remove("fixed");
           }
-          this.lastUpScrollPos = YScrollPos;
         }
       }
       this.lastYScrollPos = YScrollPos;
@@ -97,7 +91,7 @@ export default {
 <template>
 	<div class="dinamic">
 		<div class="main-user">
-			<div id="left" class="fixed"></div>
+			<div id="left" class=""></div>
 			<div class="right"></div>
 		</div>
 	</div>
@@ -122,7 +116,7 @@ export default {
 
   &.fixed {
     position: fixed;
-    //bottom: 15px;
+    //boxBottom: 15px;
   }
 }
 
