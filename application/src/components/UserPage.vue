216c5b5d26c5279de2e9970a6d6c29a553e03284
Dindro <31180301+Dindro@users.noreply.github.com>
2018-04-20 19:51:41 +0300

    плавающтй блок реализован до конца

--- application/src/components/UserPage.vue
+++ application/src/components/UserPage.vue
@@ -11,7 +11,7 @@ export default {
   methods: {
     scroll(e) {
       // условия - класс элемента не должен фиксироваться
-      const boxID = "left";
+      const boxID = "left-column";
       const boxTop = 70;
       const boxBottom = 15;
       const YScrollPos = e.currentTarget.scrollY;
@@ -30,11 +30,11 @@ export default {
       }
 
       // высота экрана больше чем высота блока
-      const isBoxOnScreen = boxFullHeight > winHeight;
+      const isBoxOnScreen = boxFullHeight < winHeight;
 
       // позиция скрола поменялась
       const isScrollChange = this.lastYScrollPos !== YScrollPos;
-      if (isBoxOnScreen && isScrollChange) {
+      if (!isBoxOnScreen && isScrollChange) {
         // скролл вниз
         const isToDown = this.lastYScrollPos < YScrollPos;
         if (isToDown) {
@@ -77,6 +77,10 @@ export default {
             box.classList.remove("fixed");
           }
         }
+      } else if (isBoxOnScreen) {
+        box.classList.add("fixed");
+        box.style.bottom = "";
+        box.style.marginTop = "";
       }
       this.lastYScrollPos = YScrollPos;
     }
@@ -84,6 +88,10 @@ export default {
   mounted() {
     window.addEventListener("scroll", this.scroll);
     window.addEventListener("resize", this.scroll);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.scroll);
+    window.removeEventListener("resize", this.scroll);
   }
 };
 </script>
@@ -91,8 +99,48 @@ export default {
 <template>
 	<div class="dinamic">
 		<div class="main-user">
-			<div id="left" class=""></div>
-			<div class="right"></div>
+			<div id="left-column">
+        <div class="user-photo mini-box">
+          <div class="photo"></div>
+          <button class="send-message">Отправить сообщение</button>
+        </div>
+        <div class="mini-box friends">
+          Избранные 43
+          <div class="friends-list">
+            <div class="friend">
+              <div class="friend-photo"></div>
+              <span class="friend-name">Дмитрий</span>
+            </div>
+            <div class="friend">
+              <div class="friend-photo"></div>
+              <span class="friend-name">Дмитрий</span>
+            </div>
+            <div class="friend">
+              <div class="friend-photo"></div>
+              <span class="friend-name">Дмитрий</span>
+            </div>
+            <div class="friend">
+              <div class="friend-photo"></div>
+              <span class="friend-name">Дмитрий</span>
+            </div>
+            <div class="friend">
+              <div class="friend-photo"></div>
+              <span class="friend-name">Дмитрий</span>
+            </div>
+            <div class="friend">
+              <div class="friend-photo"></div>
+              <span class="friend-name">Дмитрий</span>
+            </div>
+          </div>
+        </div>
+        <div class="mini-box tasks">
+          Открытые задачи 4
+          <div class="tasks-list">
+            <div class="task">Описание задачи описание</div>
+          </div>
+        </div>
+      </div>
+			<div class="right-column"></div>
 		</div>
 	</div>
 </template>
@@ -107,20 +155,78 @@ export default {
   background-color: rgb(159, 185, 185);
 }
 
-#left {
-  float: left;
-  height: 900px;
-  width: 300px;
-  background: linear-gradient(to top, #ff0000, #00ff00);
-  //position: fixed;
+.mini-box {
+  border: 1px solid rgb(230, 230, 230);
+  background-color: white;
+  border-radius: 5px;
+  margin-bottom: 15px;
+  padding: 15px;
 
-  &.fixed {
-    position: fixed;
-    //boxBottom: 15px;
+  &:last-child {
+    margin-bottom: 0;
   }
 }
 
-.right {
+#left-column {
+  float: left;
+  //height: 900px;
+  width: 230px;
+  //background: linear-gradient(to top, #ff0000, #00ff00);
+
+  // кроме position ничего добавлять
+  &.fixed {
+    position: fixed;
+  }
+
+  .user-photo {
+    .photo {
+      height: 200px;
+      background-color: violet;
+    }
+
+    .send-message {
+      width: 100%;
+      background-color: slateblue;
+      border: none;
+      margin-top: 15px;
+      border-radius: 3px;
+      padding: 10px;
+    }
+  }
+
+  .friends {
+    padding-bottom: 5px;
+
+    .friends-list {
+      padding-top: 15px;
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+    }
+
+    .friend {
+      width: 66px;
+      padding-bottom: 10px;
+
+      .friend-photo {
+        margin: 0 auto;
+        height: 50px;
+        width: 50px;
+        background-color: grey;
+        border-radius: 50%;
+      }
+
+      .friend-name {
+        text-align: center;
+        display: block;
+        margin: 0 auto;
+        margin-top: 5px;
+      }
+    }
+  }
+}
+
+.right-column {
   margin-left: 400px;
   height: 2000px;
   width: 300px;
