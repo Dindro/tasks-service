754f606a2d8b8a1f020cfb8be26610143c20c14c
Dindro <31180301+Dindro@users.noreply.github.com>
2018-04-21 15:16:37 +0300

    добавлено отызвы

--- application/src/components/UserPage.vue
+++ application/src/components/UserPage.vue
@@ -140,19 +140,160 @@ export default {
           </div>
         </div>
       </div>
-			<div class="right-column"></div>
+			<div class="right-column">
+        <div class="info mini-box">
+          <div class="name-status">
+            <span class="name">Семенов Сергей</span>
+            <span class="status">Online</span>
+          </div>
+          <div class="line after-name-status"></div>
+          <div class="profile-info">
+            <div class="profile-info-row">
+              <div class="properties">Возраст:</div>
+              <div class="description">18</div>
+            </div>
+            <div class="profile-info-row">
+              <div class="properties">Город:</div>
+              <div class="description">Чебоксары</div>
+            </div>
+            <div class="profile-info-row">
+              <div class="properties">О себе:</div>
+              <div class="description">
+                Описание описание описание описание Описание описание описание описание 
+                Описание описание описание описание Описание описание описание описание
+                Описание описание описание описание Описание описание описание описание
+                Описание описание описание описание Описание описание описание описание
+                Описание описание описание описание Описание описание описание описание
+                Описание описание описание описание Описание описание описание описание
+                Описание описание описание описание Описание описание описание описание
+              </div>
+            </div>
+            <div class="profile-info-row">
+              <div class="properties">Виды выполняемых работ:</div>
+              <div class="description">
+                <div class="work">Что то то то и его описание</div>
+                <div class="work">Что то то то и его описание</div>
+                <div class="work">Что то то то и его описание</div>
+                <div class="work">Что то то то и его описание</div>
+                <div class="work">Что то то то и его описание</div>
+                <div class="work">Что то то то и его описание</div>
+                <div class="work">Что то то то и его описание</div>
+              </div>
+            </div>
+            <div class="profile-info-row">
+              <div class="properties">Средний рейтинг:</div>
+              <div class="description">4.6</div>
+            </div>
+            <div class="profile-info-row">
+              <div class="properties">Рейтинг:</div>
+              <div class="description">
+                <div class="rating">Вежливость</div>
+                <div class="rating">Пункутальность</div>
+                <div class="rating">Адекватнось</div>
+              </div>
+            </div>
+          </div>
+          <div class="line"></div>
+          <div class="totals">
+            <div class="total">
+              <div class="count">
+                35
+              </div>
+              <div class="description">отзывов</div>
+            </div>
+            <div class="total">
+              <div class="count">
+                56
+              </div>
+              <div class="description">выполненных заданий</div>
+            </div>
+            <div class="total">
+              <div class="count">
+                4
+              </div>
+              <div class="description">созданных заданий</div>
+            </div>
+            <div class="total">
+              <div class="count">
+                10493
+              </div>
+              <div class="description">просмотров</div>
+            </div>
+          </div>
+        </div>
+        <div class="rewiews mini-box">
+          <div class="reviews-tab">
+            <div class="reviews-tab-item">
+              <span class="tab-name">Все отзывы</span>
+              <span class="tab-count">35</span>
+            </div>
+            <div class="reviews-tab-item active">
+              <span class="tab-name">От исполнителей</span>
+              <span class="tab-count">35</span>
+            </div>
+            <div class="reviews-tab-item">
+              <span class="tab-name">От заказчиков</span>
+              <span class="tab-count">35</span>
+            </div>
+          </div>
+          <div class="line"></div>
+          <div class="reviews-list">
+            <template v-for="n in 10">
+              <div class="review">
+                <div class="user">
+                  <div class="user-photo"></div>
+                  <div class="user-info">
+                    <div class="name-worker">
+                      <span class="name"></span>
+                      <span class="worker"></span>
+                    </div>
+                    <div class="rating">
+                      Рейтинг:
+                      <span class="rating-count">4.6</span>
+                    </div>
+                    <div class="review-description">
+                      Отзывы:
+                      <span class="review-count">4</span>
+                    </div>
+                  </div>
+                  <div class="time"></div>
+                </div>
+                <div class="review-task">
+                  Отзыв задания "
+                  <span class="task-name">Нужен курьер для доставки чего чего то</span>"
+                </div>
+                <div class="review-text">
+                  Тут находится отзыв Тут находится отзыв Тут находится отзыв Тут находится отзыв 
+                  Тут находится отзыв Тут находится отзыв Тут находится отзыв Тут находится отзыв 
+                  Тут находится отзыв Тут находится отзыв Тут находится отзыв Тут находится отзыв 
+                  Тут находится отзыв Тут находится отзыв Тут находится отзыв Тут находится отзыв 
+                  Тут находится отзыв Тут находится отзыв Тут находится отзыв Тут находится отзыв 
+                </div>
+                <div class="review-rating">
+                  <div class="review-rating-item">Вежливость</div>
+                  <div class="review-rating-item">Пункутальность</div>
+                  <div class="review-rating-item">Адекватнось</div>
+                </div>
+                <div class="line"></div>
+              </div>
+            </template>
+          </div>
+        </div>
+      </div>
 		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
+$color-border: #e3e4e8;
+
 .dinamic {
   float: right;
   width: 795px;
 }
 .main-user {
   margin-top: 70px;
-  background-color: rgb(159, 185, 185);
+  //background-color: rgb(159, 185, 185);
 }
 
 .mini-box {
@@ -160,11 +301,15 @@ export default {
   background-color: white;
   border-radius: 5px;
   margin-bottom: 15px;
-  padding: 15px;
+  //padding: 15px;
 
   &:last-child {
     margin-bottom: 0;
   }
+}
+
+.line {
+  border-top: 1px solid $color-border;
 }
 
 #left-column {
@@ -179,6 +324,8 @@ export default {
   }
 
   .user-photo {
+    padding: 15px;
+
     .photo {
       height: 200px;
       background-color: violet;
@@ -195,6 +342,7 @@ export default {
   }
 
   .friends {
+    padding: 15px;
     padding-bottom: 5px;
 
     .friends-list {
@@ -227,10 +375,90 @@ export default {
 }
 
 .right-column {
-  margin-left: 400px;
-  height: 2000px;
-  width: 300px;
-  background: linear-gradient(to top, #10f0ca, #cf0f5f);
+  margin-left: 245px;
+  //height: 2000px;
+  width: 550px;
+  //background: linear-gradient(to top, #10f0ca, #cf0f5f);
+
+  .info {
+    .name-status {
+      padding: 15px 20px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+
+      .name {
+        font-size: 22px;
+        font-weight: 400;
+      }
+    }
+
+    .after-name-status {
+      margin: 0 20px;
+    }
+
+    .profile-info {
+      padding: 15px 20px;
+
+      .profile-info-row {
+        padding-bottom: 10px;
+        font-size: 13px;
+
+        .properties {
+          width: 165px;
+          float: left;
+        }
+        .description {
+          margin-left: 165px;
+        }
+      }
+    }
+
+    .totals {
+      padding: 15px 20px;
+      display: flex;
+      justify-content: space-around;
+
+      .total {
+        max-width: 100px;
+
+        .count {
+          font-size: 20px;
+          text-align: center;
+        }
+
+        .description {
+          text-align: center;
+        }
+      }
+    }
+  }
+
+  .rewiews {
+
+    .reviews-tab{
+      display: flex;
+      padding: 0 10px;
+      
+      .reviews-tab-item{
+        padding: 17px 10px;
+
+        &.active{
+          border-bottom: 2px solid #4A76A8;
+          padding-bottom: 15px;
+        }
+
+        &:hover{
+          border-bottom: 2px solid rgba(74, 118, 168, 0.37);
+          padding-bottom: 15px;
+        }
+
+        .tab-count {
+          padding-left: 5px;
+        }
+      }
+    }
+  }
 }
 </style>
 
